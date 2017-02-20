53a718810a9c36226bfb6bfe1047384c8264d40c
Chris Legg <chris@navondigital.com>
2017-02-20 17:33:59 +0000

    Add subtitle to portfolio, create seeds data and display portfolio on home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217220155) do
+ActiveRecord::Schema.define(version: 20170220172419) do
 
   create_table "blogs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170217220155) do
 
   create_table "portfolios", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
+    t.string   "subtitle"
     t.text     "body",        limit: 65535
     t.text     "main_image",  limit: 65535
     t.text     "thumb_image", limit: 65535
