4025740beed8274451c145983c595ed31ef9a236
Chris Legg <chris@chrislegg.net>
2017-02-21 08:19:45 +0000

    Add custom routing for portfolio show

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   resources :blogs
 
-  root to: "pages#home"
+  root to: 'pages#home'
 end
